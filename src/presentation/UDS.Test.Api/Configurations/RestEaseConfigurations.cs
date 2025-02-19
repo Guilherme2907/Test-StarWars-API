1704176e10dbba3b5d3348bf854e7c0aa6b077b0
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-02-19 13:51:09 -0300

    feat: add restease DI configuration

---
+++ src/presentation/UDS.Test.Api/Configurations/RestEaseConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestEase;
+using UDS.Test.Repositories.Restease;
+
+namespace UDS.Test.Api.Configurations;
+
+public static class RestEaseConfigurations
+{
+    public static IServiceCollection AddRestEaseConfig(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton(RestClient.For<IStarWarsApi>(configuration["BaseAddress"]));
+
+        return services;
+    }
+}
