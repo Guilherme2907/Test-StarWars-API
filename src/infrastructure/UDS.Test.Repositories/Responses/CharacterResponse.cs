d3d1b9764f76c93f58a22b2a6d092983e1cbe4bd
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-02-19 14:17:37 -0300

    feat: add CharacterResponse

---
+++ src/infrastructure/UDS.Test.Repositories/Responses/CharacterResponse.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace UDS.Test.Repositories.Responses;
+
+public record CharacterResponse
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public double Height { get; set; }
+
+    public long Mass { get; set; }
+
+    public string Gender { get; set; }
+
+    public string Homeworld { get; set; }
+
+    public Uri Wiki { get; set; }
+
+    public Uri Image { get; set; }
+
+    public long Born { get; set; }
+
+    public string BornLocation { get; set; }
+
+    public long Died { get; set; }
+
+    public string DiedLocation { get; set; }
+
+    public IList<string> Affiliations { get; set; }
+
+    public IList<string> Masters { get; set; }
+}
