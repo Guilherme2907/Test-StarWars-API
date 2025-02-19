c3bf2c677e2f2407f44088fd68a987b11e934990
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-02-19 14:25:20 -0300

    feat: add DI for services and repositories

---
+++ src/presentation/UDS.Test.Api/Configurations/ServicesConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestEase;
+using UDS.Test.Application.Interfaces;
+using UDS.Test.Application.Services.Character;
+using UDS.Test.Domain.Models;
+using UDS.Test.Repositories.Respositories;
+using UDS.Test.Repositories.Restease;
+
+namespace UDS.Test.Api.Configurations;
+
+public static class ServicesConfigurations
+{
+    public static IServiceCollection AddServicesConfig(this IServiceCollection services)
+    {
+        services
+            .AddScoped<IStarWarRepository, StarWarRepository>()
+            .AddScoped<CharacterService, CharacterService>();
+
+        return services;
+    }
+}
