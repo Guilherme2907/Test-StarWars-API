2a4f7da862b56e859800d06c23ee284ae89159f3
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-02-19 14:16:34 -0300

    feat: add StarWarsRepository implementation

---
+++ src/infrastructure/UDS.Test.Repositories/Respositories/StarWarRepository.cs
@@ -0,0 +1,42 @@
+﻿using UDS.Test.Application.Interfaces;
+using UDS.Test.Domain.Models;
+using UDS.Test.Repositories.Restease;
+
+namespace UDS.Test.Repositories.Respositories;
+
+public class StarWarRepository : IStarWarRepository
+{
+    private readonly IStarWarsApi _starWarsApi;
+
+    public StarWarRepository(IStarWarsApi starWarsApi)
+    {
+        _starWarsApi = starWarsApi;
+    }
+
+    public async Task<IEnumerable<Character>> GetCharacteres(CancellationToken cancellationToken)
+    {
+        var apiResponse = await _starWarsApi.GetCharacteres(cancellationToken);
+
+        var characteres = apiResponse.Select(a =>
+        {
+            return new Character(
+                a.Id,
+                a.Name,
+                a.Height,
+                a.Mass,
+                a.Gender,
+                a.Homeworld,
+                a.Wiki,
+                a.Image,
+                a.Born,
+                a.BornLocation,
+                a.Died,
+                a.DiedLocation,
+                a.Affiliations,
+                a.Masters
+            );
+        });
+
+        return characteres;
+    }
+}
